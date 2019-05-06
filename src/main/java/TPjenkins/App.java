04ea6bc85ebc46028ebdbce902eed24f3e2c6760
cfulpin <cfulpin@hotmail.com>
2019-05-06 01:19:56 -0700

    maven prjct

---
+++ src/main/java/TPjenkins/App.java
@@ -0,0 +1,13 @@
+package TPjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
